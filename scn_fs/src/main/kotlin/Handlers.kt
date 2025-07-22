eb789aff21838817c0c9b520d8ceba6f9c75d4a8
Maksim Kleverov <klevermx@gmail.com>
2025-07-22 16:58:00 -0500

    bug fixes

--- scn_fs/src/main/kotlin/Handlers.kt
+++ scn_fs/src/main/kotlin/Handlers.kt
@@ -7,8 +7,6 @@ import org.litote.kmongo.deleteOneById
 import org.litote.kmongo.eq
 import org.litote.kmongo.findOne
 import org.litote.kmongo.updateOneById
-
-import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
@@ -32,7 +30,7 @@ fun insertSCDataset(path: Path,
             mongoDBCollection.insertOne(scDataset)
 
         if (scDataset.expressionFile !== null) {
-            val cur_file_size = scDataset.expressionFile.length() / (1024.0 * 1024.0)
+            val cur_file_size = File(scDataset.expressionFile).length() / (1024.0 * 1024.0)
             if (cur_file_size > max_exp_file){
                 max_exp_file = cur_file_size
             }
@@ -44,24 +42,23 @@ fun insertSCDataset(path: Path,
         if (scDataset.markersFile !== null) {
             Log.info("Updating the markers info for dataset ${scDataset.token} in the database")
             val entriesFlow = MarkerCollection.flowFromJsonFile(filePath)
-            withContext(Dispatchers.IO) {
-                log.info("Updating the markers info for dataset $token in the database")
-                mongoDBCollectionMarkers.deleteMany(SCMarkerEntry::token eq scDataset.token) // Clear previous entries
-                // Collect and process each entry
-                entriesFlow.collect { (tableName, markerEntry) ->
-                    val scMarkerEntry = SCMarkerEntry(
-                        token = scDataset.token,
-                        tableName = tableName,
-                        cluster = markerEntry.cluster,
-                        gene = markerEntry.gene,
-                        pct1 = markerEntry.pct1,
-                        pct2 = markerEntry.pct2,
-                        pValue = markerEntry.pValue,
-                        pValueAdjusted = markerEntry.pValueAdjusted,
-                        averageLogFoldChange = markerEntry.averageLogFoldChange
-                    )
-                    mongoDBCollectionMarkers.insertOne(scMarkerEntry)
-                }
+
+            Log.info("Updating the markers info for dataset ${scDataset.token} in the database")
+            mongoDBCollectionMarkers.deleteMany(SCMarkerEntry::token eq scDataset.token) // Clear previous entries
+            // Collect and process each entry
+            entriesFlow.collect { (tableName, markerEntry) ->
+                val scMarkerEntry = SCMarkerEntry(
+                    token = scDataset.token,
+                    tableName = tableName,
+                    cluster = markerEntry.cluster,
+                    gene = markerEntry.gene,
+                    pct1 = markerEntry.pct1,
+                    pct2 = markerEntry.pct2,
+                    pValue = markerEntry.pValue,
+                    pValueAdjusted = markerEntry.pValueAdjusted,
+                    averageLogFoldChange = markerEntry.averageLogFoldChange
+                )
+                mongoDBCollectionMarkers.insertOne(scMarkerEntry)
             }
             Log.info("Updated markers for ${scDataset.token}")
         }
@@ -97,7 +94,7 @@ fun insertOrUpdateSCDataset(path: Path,
 
         if (updated) {
             if (scDataset.expressionFile !== null) {
-                val cur_file_size = scDataset.expressionFile.length() / (1024.0 * 1024.0)
+                val cur_file_size = File(scDataset.expressionFile).length() / (1024.0 * 1024.0)
                 if (cur_file_size > max_exp_file){
                     max_exp_file = cur_file_size
                 }
@@ -117,26 +114,26 @@ fun insertOrUpdateSCDataset(path: Path,
 
             if (scDataset.markersFile !== null) {
                 Log.info("Updating the markers info for dataset ${scDataset.token} in the database")
-                val entriesFlow = MarkerCollection.flowFromJsonFile(filePath)
-                withContext(Dispatchers.IO) {
-                    log.info("Updating the markers info for dataset $token in the database")
-                    mongoDBCollectionMarkers.deleteMany(SCMarkerEntry::token eq scDataset.token) // Clear previous entries
-                    // Collect and process each entry
-                    entriesFlow.collect { (tableName, markerEntry) ->
-                        val scMarkerEntry = SCMarkerEntry(
-                            token = scDataset.token,
-                            tableName = tableName,
-                            cluster = markerEntry.cluster,
-                            gene = markerEntry.gene,
-                            pct1 = markerEntry.pct1,
-                            pct2 = markerEntry.pct2,
-                            pValue = markerEntry.pValue,
-                            pValueAdjusted = markerEntry.pValueAdjusted,
-                            averageLogFoldChange = markerEntry.averageLogFoldChange
-                        )
-                        mongoDBCollectionMarkers.insertOne(scMarkerEntry)
-                    }
+                val entriesFlow = MarkerCollection.flowFromJsonFile(scDataset.markersFile)
+               
+                Log.info("Updating the markers info for dataset ${scDataset.token} in the database")
+                mongoDBCollectionMarkers.deleteMany(SCMarkerEntry::token eq scDataset.token) // Clear previous entries
+                // Collect and process each entry
+                entriesFlow.collect { (tableName, markerEntry) ->
+                    val scMarkerEntry = SCMarkerEntry(
+                        token = scDataset.token,
+                        tableName = tableName,
+                        cluster = markerEntry.cluster,
+                        gene = markerEntry.gene,
+                        pct1 = markerEntry.pct1,
+                        pct2 = markerEntry.pct2,
+                        pValue = markerEntry.pValue,
+                        pValueAdjusted = markerEntry.pValueAdjusted,
+                        averageLogFoldChange = markerEntry.averageLogFoldChange
+                    )
+                    mongoDBCollectionMarkers.insertOne(scMarkerEntry)
                 }
+                
                 Log.info("Updated markers for ${scDataset.token}")
             }
         }
@@ -188,16 +185,17 @@ suspend fun fileChangeHandler(modifiedChannel: Channel<Path>,
         val fileName = modifiedPath.fileName.toString()
         val datasetDir = File(modifiedPath.toString()).parentFile.parentFile.path
         val dirName = File(modifiedPath.toString()).parentFile.name
-
-        if (fileName == DATASET_FILE_NAME) {
-            insertOrUpdateSCDataset(modifiedPath, mongoDBCollection,
-                mongoDBCollectionExp, mongoDBCollectionMarkers)
-        } else if (dirName == FILES_FOLDER_NAME) {
-            val datasetPath = Paths.get(datasetDir, DATASET_FILE_NAME)
-            Log.info(datasetPath.toString())
-            if (!Files.exists(datasetPath)) continue
-            insertOrUpdateSCDataset(datasetPath, mongoDBCollection,
-                mongoDBCollectionExp, mongoDBCollectionMarkers)
+        withContext(Dispatchers.IO) {
+            if (fileName == DATASET_FILE_NAME) {
+                insertOrUpdateSCDataset(modifiedPath, mongoDBCollection,
+                    mongoDBCollectionExp, mongoDBCollectionMarkers)
+            } else if (dirName == FILES_FOLDER_NAME) {
+                val datasetPath = Paths.get(datasetDir, DATASET_FILE_NAME)
+                Log.info(datasetPath.toString())
+                if (!Files.exists(datasetPath)) continue
+                insertOrUpdateSCDataset(datasetPath, mongoDBCollection,
+                    mongoDBCollectionExp, mongoDBCollectionMarkers)
+            }
         }
     }
 }
@@ -213,15 +211,16 @@ suspend fun fileDeleteHandler(deletedChannel: Channel<Path>,
         val fileName = deletedPath.fileName.toString()
         val dirChanged = File(deletedPath.toString()).parentFile.path
         val dirName = File(deletedPath.toString()).parentFile.name
-
-        if (fileName == DATASET_FILE_NAME) {
-            deleteSCDataset(deletedPath, mongoDBCollection,
-                mongoDBCollectionExp, mongoDBCollectionMarkers)
-        } else if (dirName == FILES_FOLDER_NAME) {
-            val datasetPath = Paths.get(dirChanged, DATASET_FILE_NAME)
-            if (!Files.exists(datasetPath)) continue
-            insertOrUpdateSCDataset(datasetPath, mongoDBCollection,
-                mongoDBCollectionExp, mongoDBCollectionMarkers)
+        withContext(Dispatchers.IO) {
+            if (fileName == DATASET_FILE_NAME) {
+                deleteSCDataset(deletedPath, mongoDBCollection,
+                    mongoDBCollectionExp, mongoDBCollectionMarkers)
+            } else if (dirName == FILES_FOLDER_NAME) {
+                val datasetPath = Paths.get(dirChanged, DATASET_FILE_NAME)
+                if (!Files.exists(datasetPath)) continue
+                insertOrUpdateSCDataset(datasetPath, mongoDBCollection,
+                    mongoDBCollectionExp, mongoDBCollectionMarkers)
+            }
         }
     }
 }
