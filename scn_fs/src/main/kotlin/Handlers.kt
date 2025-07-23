471d6569a77c08233a8ef5d01ff5048a7cebc542
Maksim Kleverov <klevermx@gmail.com>
2025-07-22 19:46:01 -0500

    flow in batches

--- scn_fs/src/main/kotlin/Handlers.kt
+++ scn_fs/src/main/kotlin/Handlers.kt
@@ -13,6 +13,7 @@ import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
+import kotlinx.coroutines.flow.flow
 var max_exp_file:Double = 0.0
 const val BATCH_SIZE = 1000
 suspend fun insertSCDataset(
@@ -208,7 +209,7 @@ suspend fun fileDeleteHandler(
 }
 }
 
-suspend fun <T> Flow<T>.collectInBatches(batchSize: Int, action: suspend (List<T>) -> Unit) {
+fun <T> Flow<T>.collectInBatches(batchSize: Int, action: suspend (List<T>) -> Unit) {
     val buffer = mutableListOf<T>()
     collect { item ->
         buffer.add(item)
--- scn_fs/src/main/kotlin/SCMarkers.kt
+++ scn_fs/src/main/kotlin/SCMarkers.kt
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.core.JsonToken
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import java.io.File
 import kotlinx.serialization.Serializable
